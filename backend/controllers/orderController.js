4ca54e16d943995c9716d8514265e93705348f0a
Brayden Lewis <braydenclewis@gmail.com>
2024-02-14 17:48:05 -0500

    Order page and fix bug with order creation not adding user

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -45,7 +45,7 @@ const addOrderItems = asyncHandler(async (req, res) => {
 // @access  Private
 const getMyOrders = asyncHandler(async (req, res) => {
   const orders = await Order.find({ user: req.user._id });
-  res.json(orders);
+  res.status(200).json(orders);
 });
 
 // @desc    Get order by ID
